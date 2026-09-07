31f45828e758d31167561f7285a11224ce6f9253
print636 <18979617895@163.com>
2026-09-07 15:17:21 +0800

    feat(frontend): add starrail study visual theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,1 +1,1 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App'; import './style.css'; import './workflow.css'; ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App'; import './style.css'; import './workflow.css'; import './starrail-study.css'; ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
